322af7f8bbeea89545273bcac9b2745a139a5a68
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2025-11-13 14:59:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5723321455-a0c4f",
+  "appId": "1:160776122958:web:677ae6646c292e76d5c91f",
+  "apiKey": "AIzaSyDM19XVjX8Xraw3uf9ZdYo_0MomdYfWsME",
+  "authDomain": "studio-5723321455-a0c4f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "160776122958"
+};
