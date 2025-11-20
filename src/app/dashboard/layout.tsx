c0965910e6c59fcf9206b1feeeda64e1330466c8
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2025-11-20 20:04:48 +0000

    quando estou expandindo o sidebar ele esta entrando na frente das inform

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -39,16 +39,16 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 
 
   return (
-    <SidebarProvider>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40 dark:bg-background">
+    <div className="min-h-screen w-full bg-muted/40 dark:bg-background">
+      <SidebarProvider>
         <AppSidebar />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 md:pl-14 transition-[margin-left] ease-in-out duration-300 md:group-[[data-sidebar-state=expanded]]/sidebar-wrapper:ml-64">
             <Header />
             <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-            {children}
+              {children}
             </main>
         </div>
-        </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </div>
   );
 }
