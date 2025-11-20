be7481daef088a55eaad2ceb307e8388e298fc50
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2025-11-20 20:02:24 +0000

    com o sidebar aberto a tela do lado direito nao deveria ficar cortada pe

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -42,7 +42,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
     <SidebarProvider>
         <div className="flex min-h-screen w-full flex-col bg-muted/40 dark:bg-background">
         <AppSidebar />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 transition-[margin-left] ease-in-out duration-300 md:group-[[data-sidebar-state=open]]/sidebar-wrapper:ml-64">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 md:pl-14 transition-[margin-left] ease-in-out duration-300 md:group-[[data-sidebar-state=expanded]]/sidebar-wrapper:ml-64">
             <Header />
             <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
             {children}
