657b9501db7c500e720a50c960832a1c2ee846c2
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2025-11-20 20:20:25 +0000

    eu acho que nao esta funcionando estamos usando o gs://studio-5723321455

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5723321455-a0c4f",
   "appId": "1:160776122958:web:677ae6646c292e76d5c91f",
-  "storageBucket": "studio-5723321455-a0c4f.appspot.com",
+  "storageBucket": "studio-5723321455-a0c4f.firebasestorage.app",
   "apiKey": "AIzaSyDM19XVjX8Xraw3uf9ZdYo_0MomdYfWsME",
   "authDomain": "studio-5723321455-a0c4f.firebaseapp.com",
   "measurementId": "",
