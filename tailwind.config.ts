4ca9764ed2ce1d910a16e1a2e0a352215b63074b
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2025-12-18 13:25:10 +0000

    usar Rubik Glitch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        headline: ['"Rubik Glitch"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
