fd4a35d8391f4ef5c6fc7859ceb0174e2ef97027
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2026-01-06 16:59:50 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
