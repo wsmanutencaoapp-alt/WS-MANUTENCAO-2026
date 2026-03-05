1a92945d7e923e0ef2739ee5626e2d4c5b84d2a0
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2026-03-05 14:57:48 +0000

    fiz um teste aqui desligando a internet, o sistema cai
    Não é possível a

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
 });
 
 const nextConfig = {
