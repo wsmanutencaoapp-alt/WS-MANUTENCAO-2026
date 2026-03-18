eb0fb4565fb7a5a63447cfa78afbec65061d59a3
WS MANUTENCAO <wsmanutencaoapp@gmail.com>
2026-03-18 18:29:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  cacheOnFrontEndNav: true,
+  cacheOnFrontEndNav: false,
   aggressiveFrontEndNavCaching: false,
 });
 
