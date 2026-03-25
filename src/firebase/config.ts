9703cadfad0b0ddc8e0a04adb30ecab953b906ce
Angelo Ricardo Dallagnol <angeloricardodallagnol@MacBook-Air-de-Angelo.local>
2026-03-25 12:48:41 -0300

    Fix firebase config project mismatch and redeploy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5723321455-a0c4f",
-  "appId": "1:160776122958:web:677ae6646c292e76d5c91f",
-  "storageBucket": "studio-5723321455-a0c4f.firebasestorage.app",
-  "apiKey": "AIzaSyDM19XVjX8Xraw3uf9ZdYo_0MomdYfWsME",
-  "authDomain": "studio-5723321455-a0c4f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "160776122958"
+  "projectId": "aerotrack-leo-05363303-5fc98",
+  "appId": "1:584792250553:web:271c77ed9300b8c6c72368",
+  "storageBucket": "aerotrack-leo-05363303-5fc98.firebasestorage.app",
+  "apiKey": "AIzaSyDSUVSwwjY3swTTaZdIJpf6lqG0ScFQbyY",
+  "authDomain": "aerotrack-leo-05363303-5fc98.firebaseapp.com",
+  "messagingSenderId": "584792250553"
 };
